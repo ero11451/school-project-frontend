398b027ab8d9f586dffae2724cf9f1d248116866
muyi <eroosamuyi11451@gmail.com>
2024-12-02 01:38:39 +0000

    remove the react

--- src/pages/DevToolsPage.tsx
+++ src/pages/DevToolsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import documentation from "./documentation.json";
 import { Documentation } from "../interface/IdevTool";
 
